9009508dcdb22224168884f83352acd7e9090454
Ji Seungmin <cswcsm02@gmail.com>
2023-03-07 22:46:28 +0900

    Feat: DB 커넥션 연결 기능 추가

--- src/main/java/com/spring/monitoring/controller/TrafficController.java
+++ src/main/java/com/spring/monitoring/controller/TrafficController.java
@@ -1,9 +1,13 @@
 package com.spring.monitoring.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,6 +35,18 @@ public class TrafficController {
         for (int i = 0; i < 1000000; i++) {
             list.add("hello jvm!" + i);
         }
+        return "ok";
+    }
+
+    //커넥션 연결
+    @Autowired
+    DataSource dataSource;
+    @GetMapping("/jdbc")
+    public String jdbc() throws SQLException {
+        log.info("jdbc");
+        Connection conn = dataSource.getConnection();
+        log.info("connection info={}", conn);
+        //conn.close(); //커넥션을 닫지 않는다.
         return "ok";
     }
 }
