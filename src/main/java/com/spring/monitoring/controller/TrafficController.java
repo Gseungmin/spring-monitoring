9b6fce98806734d4e6a54ec65068b5d23aacf82b
Ji Seungmin <cswcsm02@gmail.com>
2023-03-07 22:39:37 +0900

    Feat: 메모리 사용량 테스트 기능 추가

--- src/main/java/com/spring/monitoring/controller/TrafficController.java
+++ src/main/java/com/spring/monitoring/controller/TrafficController.java
@@ -4,11 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 public class TrafficController {
 
 
+    //cpu 사용량 측정
     @GetMapping("/cpu")
     public String cpu() {
         log.info("cpu");
@@ -17,5 +21,16 @@ public class TrafficController {
             value++;
         }
         return "ok value=" + value;
+    }
+
+    //메모리 사용량 측정
+    private List<String> list = new ArrayList<>();
+    @GetMapping("/jvm")
+    public String jvm() {
+        log.info("jvm");
+        for (int i = 0; i < 1000000; i++) {
+            list.add("hello jvm!" + i);
+        }
+        return "ok";
     }
 }
